372b9ca638663430e51f96e788e524f103748358
Jonathan <Jonathan@Jonathan-PC.fritz.box>
2021-09-02 18:24:03 +0200

    add peer dependencies to package.json

--- Controller.v2/src/config/sample.config.getSID.php
+++ Controller.v2/src/config/sample.config.getSID.php
@@ -3,4 +3,5 @@ $fb_username = "Username";
 $fb_password = "Password"; 
 $fb_loginurl = "https://xyz.myfritz.net:00000/login_sid.lua"; 
 $path = "pathof/sid.txt";
+$encryptkey = "LenghofKeyshouldbe16";
 ?>
